33c2e95f70923ae864b639f8de24d50197e4eb8c
ItsGetWreckt <samanyu.rayala@gmail.com>
2025-12-16 18:01:54 -0800

    Initial commit

---
+++ src/main/java/com/therealsam/convenientminecraft/ConvenientMinecraft.java
@@ -0,0 +1,16 @@
+package com.therealsam.convenientminecraft;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ConvenientMinecraft implements ModInitializer {
+	public static final String MOD_ID = "convenientminecraft";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/therealsam/convenientminecraft/ConvenientMinecraftClient.java
@@ -0,0 +1,10 @@
+package com.therealsam.convenientminecraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ConvenientMinecraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
